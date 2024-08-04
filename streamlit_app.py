47c263a9ffbcacf60dac9b38990ac00257910df1
buntcind <159984966+buntcind@users.noreply.github.com>
2024-08-04 19:27:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write(
 
 cnx=st.connection("snowflake");
 session=cnx.session;
-my_dataframe = cnx.session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = cnx.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
